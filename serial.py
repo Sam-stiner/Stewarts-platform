930f2a1c189ba0b11c70ea1518ebd32d534df534
Sam-stiner <sam.stiner@outlook.com>
2016-01-09 14:16:17 -0700

    Update serial.py

--- serial.py
+++ serial.py
@@ -1,5 +1,5 @@
 import serial
-arduino = serial.Serial('COM13', 115200, timeout=.1)
+arduino = serial.Serial('COM13', 9600, timeout=.1)
 while True:
 	data = arduino.readline()[:-2] #the last bit gets rid of the new-line chars
 	if data:
